dd3f9f17355dbceb0cb9ec96fde937e846cf6e02
Oleg Proskurin <proskur@altlinux.org>
2023-12-18 21:00:03 +0300

    processing command line parameters

--- main.cpp
+++ main.cpp
@@ -6,28 +6,30 @@
 
 int main(int argc, char* argv[]) {
     Options opts(argc,argv);
-    if (opts.wrong_params){
-        return -1;
-    }
+    // wrong parameters or --help
+    if (opts.wrong_params) return -1;
     if (opts.Help()) return 0;
     std::string path =opts.GetPath();
-    if (path.empty()){
-        std::cerr << "You need to specify path to repository" << std::endl;
-        return 0;
-    }
+    if (path.empty()) return 0;
     std::string url =opts.GetUrl();
     std::optional<std::vector<std::string>> excludes=opts.GetExludes();
     if (!excludes) return -1;
-
+    std::string pkg_version=opts.GetVersion();
+    if (pkg_version.empty()) return 0;
 
     Git2 git;
     // open repo or git clone if url is defined
     if (!git.Open(path)){
         if (!url.empty())
             git.Clone(url, path);
+        else{
+            std::cerr << "Can't open repo at path " << path <<std::endl
+                      << "Can't clone because you didn't path a git URL" <<std::endl;
+            return 0;
+        }
     }
 
-    git.GetSubmodules();
+    git.GetSubmodules(excludes.value());
     std::cout << "\nTotal submodules updated : "
               << git.total_submodules_updated
               << std::endl;
@@ -39,7 +41,7 @@ int main(int argc, char* argv[]) {
                   << " submodules (see WARINGS) " << std::endl;
 
     // this strategy creating no mess in files but creating terrible mess in git
-    git.CreateTags("0.10.2");
+    git.CreateTags(pkg_version);
 
     return 0;
 }
