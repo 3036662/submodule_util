e7d1d9e44efcc02846908e6e2a63301725db5b60
Oleg Proskurin <proskur@altlinux.org>
2023-12-18 13:40:59 +0300

    fix some memory leaks

--- git2.cpp
+++ git2.cpp
@@ -12,17 +12,17 @@ Git2::Git2(){
 }
 
 Git2::~Git2(){
-    if (ptrRootRepo){
-        git_repository_free(ptrRootRepo);
+    if (ptr_root_repo){
+        git_repository_free(ptr_root_repo);
     }
     git_libgit2_shutdown();
 }
 
 
-bool Git2::open(const std::string& path){
+bool Git2::Open(const std::string& path){
     if (path.empty()) return false;
-    repoPath =path;
-    int error = git_repository_open(&ptrRootRepo, repoPath.c_str());
+    repo_path =path;
+    int error = git_repository_open(&ptr_root_repo, repo_path.c_str());
     if (error){
         std::cerr<< std::endl << "Failed opening repo at  " <<path << std::endl;
         std::cerr << git_error_last()->message << std::endl;
@@ -31,9 +31,9 @@ bool Git2::open(const std::string& path){
     return true;
 }
 
-bool Git2::clone(const std::string& upstreamUrl, const std::string& path){
+bool Git2::Clone(const std::string& upstreamUrl, const std::string& path){
     if (upstreamUrl.empty() || path.empty()) return false;
-    int error= git_clone(&ptrRootRepo, upstreamUrl.c_str(), path.c_str(), NULL);
+    int error= git_clone(&ptr_root_repo, upstreamUrl.c_str(), path.c_str(), NULL);
     if  (error){
         std::cerr << "error while performing git clone "<< upstreamUrl  << std::endl;
          std::cerr << git_error_last()->message << std::endl;
@@ -42,11 +42,11 @@ bool Git2::clone(const std::string& upstreamUrl, const std::string& path){
     return true;
 }
 
-std::vector<Submodule> Git2::getSubmodules(){    
-    return getSubmodules(ptrRootRepo,"");
+std::vector<Submodule> Git2::GetSubmodules(){
+    return GetSubmodules(ptr_root_repo,"");
 }
 
-std::vector<Submodule> Git2::getSubmodules(git_repository* ptrRepo, const std::string& parent_dir){
+std::vector<Submodule> Git2::GetSubmodules(git_repository* ptrRepo, const std::string& parent_dir){
     std::vector<Submodule> res;
     err_count=0;
     if (!ptrRepo){return res;}
@@ -73,23 +73,23 @@ std::vector<Submodule> Git2::getSubmodules(git_repository* ptrRepo, const std::s
     }
     // init + update
     for (auto it_sm=res.begin();it_sm != res.end(); ++it_sm){
-        std::cout << *it_sm;
-        it_sm->init();
-        if (it_sm->update(false))
+       std::cout << *it_sm;
+       it_sm->Init();
+       if (it_sm->Update(false))
            ++total_submodules_updated;
-        else{
+       else{
             std::cerr << "[WARINIG] Couldn't update " << it_sm->name << std::endl;
             ++err_count;
         }
         // recursive call - put submoduler submodule.subvec
         if (curr_recursion_depth<max_recursion_depth){
             ++curr_recursion_depth;
-            it_sm->subvec = getSubmodules(it_sm->getRepo(),it_sm->absolute_path);
+            it_sm->subvec = GetSubmodules(it_sm->GetRepo(),it_sm->absolute_path);
             --curr_recursion_depth;
             if (!it_sm->subvec.empty())
                 ++recursion_depth_reached;
         }
-        it_sm->freeRepo();
+        it_sm->FreeRepo();
     }
     submodules=res;
     return res;
@@ -126,7 +126,7 @@ void Git2::CreateTagsRecursive(const std::vector<Submodule>& vec, const std::str
 }
 
 
-bool Git2::createTags(const std::string& version){
+bool Git2::CreateTags(const std::string& version){
     // for each submodule create tag
     std::vector<Tag> res;
     std::vector<Submodule> subm_failed;
@@ -189,14 +189,13 @@ bool Git2::createTags(const std::string& version){
 int Git2::SubmouduleForeachCallbackC(git_submodule *sm,  const char *name_, void *ptrResultVector){
     if (!sm || ! ptrResultVector || !name_) return -1;
     std::vector<Submodule>* ptrRes=static_cast<std::vector<Submodule>*> (ptrResultVector);
-
     //head oid or NULL
     const git_oid* head_oid=git_submodule_head_id(sm);
     char buffer[100]={0};
     // head hash 40 symbols
     std::string head_oid_Str;
     if (head_oid){
-        head_oid_Str=git_oid_tostr(buffer,100,head_oid);
+        head_oid_Str=git_oid_tostr(buffer,99,head_oid);
     }
     // name
     std::string name(name_);
@@ -212,7 +211,7 @@ int Git2::SubmouduleForeachCallbackC(git_submodule *sm,  const char *name_, void
     if (ptr_url){
         url=ptr_url;
     }
-    ptrRes->emplace_back(sm,head_oid,std::move(head_oid_Str),std::move(name),std::move(path),std::move(url));
+   ptrRes->emplace_back(nullptr,head_oid,std::move(head_oid_Str),std::move(name),std::move(path),std::move(url));
     return 0;
 }
 
@@ -226,7 +225,7 @@ bool Git2::FetchRemote(const Submodule& sm){
     }
     git_remote* tmpRemote = nullptr;
     std::string remote_name= sm.name;
-    int error =git_remote_create(&tmpRemote,ptrRootRepo,remote_name.c_str(),sm.absolute_url.c_str());
+    int error =git_remote_create(&tmpRemote,ptr_root_repo,remote_name.c_str(),sm.absolute_url.c_str());
     if (error){
         std::cerr << "Error creating remote for url " << sm.absolute_url << std::endl;
         PrintLastError();
@@ -240,11 +239,10 @@ bool Git2::FetchRemote(const Submodule& sm){
     if (error) {
         std::cerr << "Error fetching remote for url " << sm.absolute_url << std::endl;
         PrintLastError();
-        return false;
     }
     // detete remote
     git_remote_free(tmpRemote);
-    error = git_remote_delete(ptrRootRepo,remote_name.c_str());
+    error = git_remote_delete(ptr_root_repo,remote_name.c_str());
     if (error){
         std::cerr << "Can't delete remote "<<sm.name << std::endl;
         PrintLastError();
@@ -256,38 +254,37 @@ bool Git2::FetchRemote(const Submodule& sm){
 bool Git2::CreateTag(const Submodule& sm, const std::string& tag_name){
     //find oid for hsa
     git_oid oid;
-    int error = git_oid_fromstr(&oid,sm.headOid.c_str());
+    int error = git_oid_fromstr(&oid,sm.head_oid.c_str());
     if (error){
-      std::cerr << "Can't get oid from sha "<<sm.headOid << std::endl;
+      std::cerr << "Can't get oid from sha "<<sm.head_oid << std::endl;
       PrintLastError();
       return 0;
     }
     // find object by oid
     git_object* ptr_tmp_obj=nullptr;
-    error = git_object_lookup (&ptr_tmp_obj,ptrRootRepo,&oid,GIT_OBJECT_ANY);
+    error = git_object_lookup (&ptr_tmp_obj,ptr_root_repo,&oid,GIT_OBJECT_ANY);
     if (error){
         std::cerr << "Can't find object "<<sm.name << std::endl;
         PrintLastError();
         return 0;
     }
     // create tag
-    git_oid& created_tag_oid=oid ;
-    error = git_tag_create_lightweight (&created_tag_oid, ptrRootRepo,tag_name.c_str(),ptr_tmp_obj,1);
+    git_oid created_tag_oid;
+    error = git_tag_create_lightweight (&created_tag_oid, ptr_root_repo,tag_name.c_str(),ptr_tmp_obj,1);
+    git_object_free(ptr_tmp_obj);
     if (error){
         std::cerr << "Can't create tag "<< tag_name << std::endl;
-        std::cerr << git_error_last()->message << std::endl;
-        git_object_free(ptr_tmp_obj);
+        PrintLastError();
         return false;
     }
-    git_object_free(ptr_tmp_obj);
     return true;
 }
 
 bool Git2::MergeTag(const std::string& tag_name){
 
     // reference to tag
-    git_reference * tag_reference;
-    int error = git_reference_lookup(&tag_reference,ptrRootRepo,
+    git_reference * tag_reference=nullptr;
+    int error = git_reference_lookup(&tag_reference,ptr_root_repo,
                                      (std::string("refs/tags/") + tag_name).c_str());
     if (error){
       std::cerr << "Can't find refference to tag" << std::endl;
@@ -295,12 +292,13 @@ bool Git2::MergeTag(const std::string& tag_name){
     }
     // annotated commit
     git_annotated_commit* tag_commit;
-    error = git_annotated_commit_from_ref(&tag_commit, ptrRootRepo, tag_reference);
+    error = git_annotated_commit_from_ref(&tag_commit, ptr_root_repo, tag_reference); //FREE
     if (error){
         std::cerr << "Can't create annotated commit from refference to tag" << std::endl;
         std::cerr << git_error_last()->message << std::endl;
         return false;
     }
+    git_reference_free(tag_reference);
     // perfom merge
     std::cout << "Started merging " << tag_name << std::endl;
     git_merge_options merge_opts = GIT_MERGE_OPTIONS_INIT;
@@ -308,15 +306,13 @@ bool Git2::MergeTag(const std::string& tag_name){
     merge_opts.file_favor=GIT_MERGE_FILE_FAVOR_OURS;
     git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
     checkout_opts.checkout_strategy =GIT_CHECKOUT_USE_OURS | GIT_CHECKOUT_DONT_UPDATE_INDEX ;
-    git_merge(ptrRootRepo,(const git_annotated_commit **) &tag_commit, 1, &merge_opts, &checkout_opts);
+    git_merge(ptr_root_repo,(const git_annotated_commit **) &tag_commit, 1, &merge_opts, &checkout_opts);
     if (error){
       std::cerr << "Can't  merge " << tag_name << std::endl;
       PrintLastError();
       git_annotated_commit_free(tag_commit);
       return false;
     }
-    //git_annotated_commit_free(tag_commit); TODO Free
-
 
     // COMMIT
 
@@ -326,10 +322,9 @@ bool Git2::MergeTag(const std::string& tag_name){
         PrintLastError();
         return false;
     }
-
     // get index for repo
     git_index *index;
-    error =git_repository_index(&index, ptrRootRepo);
+    error =git_repository_index(&index, ptr_root_repo);
     if (error){
         std::cerr << "Can't find repository index " << std::endl;
         PrintLastError();
@@ -337,7 +332,7 @@ bool Git2::MergeTag(const std::string& tag_name){
     }
     // get reference to head
     git_reference *head_ref;
-    git_repository_head(&head_ref, ptrRootRepo); // TODO free
+    git_repository_head(&head_ref, ptr_root_repo);
     if (error){
         std::cerr << "Can't find reference to head "<<std::endl;
         PrintLastError();
@@ -352,9 +347,10 @@ bool Git2::MergeTag(const std::string& tag_name){
         PrintLastError();
         return false;
     }
+    git_index_free(index);
     // get tree* from tree_oid
     git_tree *tree;
-    git_tree_lookup(&tree, ptrRootRepo, &tree_oid);
+    git_tree_lookup(&tree, ptr_root_repo, &tree_oid);
     if (error){
         std::cerr << "failed to lookup tree " << std::endl;
         PrintLastError();
@@ -373,7 +369,8 @@ bool Git2::MergeTag(const std::string& tag_name){
     //create git_commit* from git_annotated_commit* (merge_annotated_commit)
     const git_oid* annotated_oid=git_annotated_commit_id(tag_commit);
     git_commit * merge_commit=nullptr;
-    error=git_commit_lookup(&merge_commit,ptrRootRepo,annotated_oid); // TODO free git_commit_free
+    error=git_commit_lookup(&merge_commit,ptr_root_repo,annotated_oid); // TODO free git_commit_free
+    git_annotated_commit_free(tag_commit);
     if (error){
         PrintLastError();
         return false;
@@ -384,7 +381,7 @@ bool Git2::MergeTag(const std::string& tag_name){
     std::string commit_mesage= "Merge " + tag_name;
     git_oid commit_oid;
     error = git_commit_create(&commit_oid,
-                              ptrRootRepo,
+                              ptr_root_repo,
                               git_reference_name(head_ref), //name of HEAD
                               author,
                               author,
@@ -394,12 +391,17 @@ bool Git2::MergeTag(const std::string& tag_name){
                               2,
                              parent
                               );
+    git_signature_free(author);
+    git_reference_free(head_ref);
+    git_commit_free(head_commit);
+    git_tree_free(tree);
+    git_commit_free(merge_commit);
     if (error){
         std::cerr << "failed to create commit for merge " << std::endl;
         PrintLastError();
         return false;
     }
-    git_repository_state_cleanup(ptrRootRepo);
+    git_repository_state_cleanup(ptr_root_repo);
     return true;
 }
 
