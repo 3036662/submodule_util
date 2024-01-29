e932637d61fa4884122382c57ebe4f67a912e164
Oleg Proskurin <proskur@altlinux.org>
2024-01-29 17:44:38 +0300

    add comments

--- main.cpp
+++ main.cpp
@@ -7,18 +7,32 @@
 #include "submodule.hpp"
 
 int main(int argc, char* argv[]) {
+    /* ------------------------------------
+     * Parse programm options
+     *  ------------------------------------ */
+
     Options opts(argc, argv);
     // wrong parameters or --help
     if (opts.wrong_params) return -1;
     if (opts.Help()) return 0;
+
+    // path - required
     std::string path = opts.GetPath();
     if (path.empty()) return 0;
+
+    // url - optional
     std::string url = opts.GetUrl();
+
+    // exclude list if false - error oppening exclude list - exit
     std::optional<std::vector<std::string>> excludes = opts.GetExludes();
     if (!excludes) return -1;
+
+    // package version - required
     std::string pkg_version = opts.GetVersion();
     if (pkg_version.empty()) return 0;
+
     Git2 git;
+
     // override commiter if passed
     std::optional<std::pair<std::string, std::string>> override_committer =
         opts.GetCommitter();
@@ -29,7 +43,9 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    //-------------------------------------------------
     // open repo or git clone if url is defined
+
     if (!git.Open(path)) {
         if (!url.empty())
             git.Clone(url, path);
@@ -41,6 +57,9 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // -------------------------------------------------
+    // Init and update submodules
+
     git.GetSubmodules(excludes.value());
     std::cout << "\nTotal submodules updated : " << git.total_submodules_updated
               << std::endl;
@@ -50,9 +69,12 @@ int main(int argc, char* argv[]) {
         std::cerr << "!!! \nUpdate FAILED for " << git.err_count
                   << " submodules (see WARINGS) " << std::endl;
 
+    // --------------------------------------------------
+    // Create tags and merge
     // this strategy creating no mess in files but creating terrible mess in git
-    if (!opts.var_map.count("only-update")){
+    if (!opts.var_map.count("only-update")) {
         git.CreateTags(pkg_version);
     }
+
     return 0;
 }
